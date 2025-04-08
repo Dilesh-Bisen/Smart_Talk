89a8049487ad222ec445d40e6d4fcd05c39df797
Dilesh Bisen <2dileshbisen@gmail.com>
2025-04-08 19:01:33 +0530

    update-file

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.post('/register-user', async (req: Request, res: Response): Promise<any> => {
-    res.send('test');
+    // res.send('Testing 1 2 3 okk');
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({ error: 'Name and email are required' });
+    }
+    res.status(200).json({ message: "Successfully Executed" });
 });
 
 const PORT = process.env.PORT || 3000;
